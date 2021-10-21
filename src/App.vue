9c1e7db8dad8d151dfd5896218bab34e90fcabfd
aManOf502 <1954773596@qq.com>
2021-10-21 22:28:45 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+<!--    <v-app-bar-->
+<!--        app-->
+<!--        color="white"-->
+<!--        flat-->
+<!--    >-->
+<!--      <v-avatar-->
+<!--          :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"-->
+<!--          size="32"-->
+<!--      ></v-avatar>-->
+
+<!--      <v-tabs-->
+<!--          centered-->
+<!--          class="ml-n9"-->
+<!--          color="grey darken-1"-->
+<!--      >-->
+<!--        <v-tab-->
+<!--            v-for="link in links"-->
+<!--            :key="link"-->
+<!--        >-->
+<!--          {{ link }}-->
+<!--        </v-tab>-->
+<!--      </v-tabs>-->
+
+<!--      <v-avatar-->
+<!--          class="hidden-sm-and-down"-->
+<!--          color="grey darken-1 shrink"-->
+<!--          size="32"-->
+<!--      ></v-avatar>-->
+<!--    </v-app-bar>-->
+    <Header></Header>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+              cols="12"
+              sm="2"
+          >
+            <v-sheet
+                rounded="lg"
+                min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+              cols="12"
+              sm="8"
+          >
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+              cols="12"
+              sm="2"
+          >
+            <v-sheet
+                rounded="lg"
+                min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/Header";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Header},
+  data: () => ({
+    links: [
+      '首页',
+      '技术论坛',
+      '知识点',
+      'Updates',
+    ],
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app-bar
+      app
+      color="white"
+      flat
+  >
+    <v-tabs
+        class="ml-n9 margin-left"
+        color="grey darken-1"
+        centered
+    >
+      <v-tab
+          v-for="link in links"
+          :key="link"
+      >
+        {{ link }}
+      </v-tab>
+
+    </v-tabs>
+
+    <v-input>message</v-input>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-avatar
+            color="orange"
+            size="62"
+            v-bind="attrs"
+            v-on="on"
+        >
+          <span class="white--text text-h5">62</span>
+        </v-avatar>
+      </template>
+      <v-list>
+        <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+name: "Header",
+  data: () => ({
+    links: [
+      '首页',
+      '技术论坛',
+      '知识点',
+      'Updates',
+    ],
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+ .headerTags{
+   margin-left: 500px;
+ }
+</style>
